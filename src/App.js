e4543e6d4e5eaf962363c548384f0330b9e41ff0
VR314 <raghupathivaroon@gmail.com>
2021-05-27 11:24:38 -0400

    add geojson to /public, add resizing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Globe from 'react-globe.gl';
+import React from "react";
+import Globe from "react-globe.gl";
 
 const { useState, useEffect, useRef } = React;
 
@@ -13,53 +13,37 @@ export default function World() {
     400,
     600,
     800, */
-    1000,
-    1279,
-    1492,
-    1530,
-    1650,
-    1715,
-    1783,
-    1815,
-    1880,
-    1914,
-    1920,
-    1938,
-    1945,
-    1994,
-    2021
+    1000, 1279, 1492, 1530, 1650, 1715, 1783, 1815, 1880, 1914, 1920, 1938,
+    1945, 1994, 2021,
   ];
   const [year, setYear] = useState(1492);
+  const [stage, setStage] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
   useEffect(() => {
-    if (year === 2021) {
-      fetch(`./custom.geo.json`)
-        .then(res => res.json())
-        .then(countries => {
-          globeEl.current.pauseAnimation();
-          setCountries(countries);
+    // load map data
+    fetch(`./world_${year}.geojson`)
+      .then((res) => res.json())
+      .then((countries) => {
+        globeEl.current.pauseAnimation();
+        setCountries(countries);
 
-          setTimeout(() => {
-            globeEl.current.resumeAnimation();
-            setTransitionDuration(2000);
-          }, 1500);
-        });
-    } else {
-      // load data
-      fetch(
-        `https://raw.githubusercontent.com/aourednik/historical-basemaps/master/world_${year}.geojson`
-      )
-        .then(res => res.json())
-        .then(countries => {
-          globeEl.current.pauseAnimation();
-          setCountries(countries);
-
-          setTimeout(() => {
-            globeEl.current.resumeAnimation();
-            setTransitionDuration(2000);
-          }, 1500);
-        });
-    }
+        setTimeout(() => {
+          globeEl.current.resumeAnimation();
+          setTransitionDuration(2000);
+        }, 1500);
+      });
   }, [year]);
 
   /*
@@ -81,91 +65,111 @@ export default function World() {
   }, []);
 
   return (
-    <div>
-      <div
-        style={{
-          position: 'absolute',
-          height: '80vh',
-          width: '30vw',
-          zIndex: 1,
-          backgroundColor: 'rgba(0,206,209,0.25)'
-        }}
-      >
-        <h1
+    <>
+      <div>
+        <div
           style={{
-            position: 'absolute',
-            color: 'white',
-            margin: '5% 15%',
-            width: 'auto'
+            position: "absolute",
+            height: "80vh",
+            width: "30vw",
+            zIndex: 1,
+            backgroundColor: "rgba(0,206,209,0.25)",
           }}
-        >{`The World in ${year}`}</h1>
-        {year !== possibleYears[0] && (
-          <button
+        >
+          <div
             style={{
-              position: 'absolute',
-              marginTop: '70vh',
-              marginLeft: '2.5vw',
-              height: '10%',
-              width: '2.5vw'
+              position: "absolute",
+              color: "white",
+              margin: "5% 10%",
             }}
-            onClick={() =>
-              setYear(possibleYears[possibleYears.indexOf(year) - 1])
-            }
           >
-            {'<'}
-          </button>
-        )}
-        {year !== possibleYears[possibleYears.length - 1] && (
-          <button
-            style={{
-              position: 'absolute',
-              marginTop: '70vh',
-              marginLeft: '25vw',
-              height: '10%',
-              width: '2.5vw'
-            }}
-            onClick={() =>
-              setYear(possibleYears[possibleYears.indexOf(year) + 1])
-            }
-          >
-            {'>'}
-          </button>
-        )}
-      </div>
-      <Globe
-        ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-        polygonsData={countries.features.filter(
-          d => d.properties.ISO_A2 !== 'AQ'
-        )}
-        polygonCapColor={({ properties: d }) => {
-          if (colors) {
-            if (
-              d.NAME === 'unclaimed' ||
-              d.NAME === 'Africa' ||
-              d.NAME === 'undefined'
-            ) {
-              return `rgba(1,1,1, 0.9)`;
-            }
-            return `hsla(${~~(360 * Math.random())},70%,70%,0.8)`;
+            <h1
+              style={{
+                width: "auto",
+                fontFamily: "Verdana",
+                fontSize: "1.75em",
+              }}
+            >{`The World in ${year}`}</h1>
+            <p
+              style={{
+                position: "absolute",
+                color: "white",
+                marginTop: 0,
+              }}
+            >
+              {"this is some sample text"}
+            </p>
+          </div>
+          {year !== possibleYears[0] && (
+            <button
+              style={{
+                position: "absolute",
+                marginTop: "70vh",
+                marginLeft: "2.5vw",
+                height: "10%",
+                width: "2.5vw",
+              }}
+              onClick={() =>
+                setYear(possibleYears[possibleYears.indexOf(year) - 1])
+              }
+            >
+              {"<"}
+            </button>
+          )}
+          {year !== possibleYears[possibleYears.length - 1] && (
+            <button
+              style={{
+                position: "absolute",
+                marginTop: "70vh",
+                marginLeft: "25vw",
+                height: "10%",
+                width: "2.5vw",
+              }}
+              onClick={() =>
+                setYear(possibleYears[possibleYears.indexOf(year) + 1])
+              }
+            >
+              {">"}
+            </button>
+          )}
+        </div>
+        <Globe
+          ref={globeEl}
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+          polygonsData={countries.features.filter(
+            (d) => d.properties.ISO_A2 !== "AQ"
+          )}
+          polygonCapColor={({ properties: d }) => {
+            if (colors) {
+              if (
+                d.NAME === "unclaimed" ||
+                d.NAME === "Africa" ||
+                d.NAME === "undefined"
+              ) {
+                return `rgba(1,1,1, 0.9)`;
+              }
+              return `hsla(${~~(360 * Math.random())},70%,70%,0.8)`;
 
-            var keys = Object.keys(colors);
-            let rgb = colors[keys[(keys.length * Math.random()) << 0]];
-            return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.7)`;
-          }
-        }}
-        polygonAltitude={0.005}
-        polygonLabel={({ properties: d }) => `
+              var keys = Object.keys(colors);
+              let rgb = colors[keys[(keys.length * Math.random()) << 0]];
+              return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.7)`;
+            }
+          }}
+          polygonAltitude={0.005}
+          polygonLabel={({ properties: d }) => `
         <b>${
-          d.NAME === 'unclaimed' ||
-          d.NAME === 'Africa' ||
-          d.NAME === 'undefined'
-            ? ''
+          d.NAME === "unclaimed" ||
+          d.NAME === "Africa" ||
+          d.NAME === "undefined"
+            ? ""
             : d.NAME
         }</b> 
       `}
-        polygonsTransitionDuration={transitionDuration}
-      />
-    </div>
+          polygonsTransitionDuration={transitionDuration}
+          height={height}
+          width={width}
+        />
+      </div>
+    </>
   );
 }
